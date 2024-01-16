fb48939ce7695f5d9613fba0f618fbee30f6bdac
Samin Sarker <saminsarker05@gmail.com>
2024-01-16 01:12:00 -0500

    able to send POST request from form to backend

--- bot/core/views.py
+++ bot/core/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
 def chatbot(request):
+  if request.method == 'POST':
+    message = request.POST.get('message')
+    response = "HOLA"
+    return JsonResponse({"message": message, "response": response})
   return render(request, 'home.html')
