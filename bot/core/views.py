ab295ff8487094060731152b6af03cbf1d05290c
Samin Sarker <saminsarker05@gmail.com>
2024-01-16 01:33:34 -0500

    openai connection not working

--- bot/core/views.py
+++ bot/core/views.py
@@ -1,9 +1,29 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from openai import OpenAI
+
+OPENAI = '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'
+client = OpenAI(api_key = OPENAI)
+
+def process(message):
+  response = client.chat.completions.create(
+    model="gpt-3.5-turbo",
+    messages=[
+
+      {"role": "system", "content": "You are a helpful assistant."},
+      {"role": "user", "content": message},
+    ]
+  )
+  print(response)
+  answer = response.choices[0].strip()
+  return answer
+
+
 
 def chatbot(request):
   if request.method == 'POST':
     message = request.POST.get('message')
-    response = "HOLA"
+    response = "TEST MESSAGE"
+    # response = process(message)
     return JsonResponse({"message": message, "response": response})
   return render(request, 'home.html')
